76cbf82dbb24415c593b09de26150d5ff2c15ebb
Oğuzhan <oguzhan.turan@fibabanka.com.tr>
2021-02-26 20:03:34 +0300

    first commit

---
+++ src/main/java/com/project/petclinic/PetClinicApplication.java
@@ -0,0 +1,14 @@
+package com.project.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//command + shif + o auto import
+@SpringBootApplication
+public class PetClinicApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(PetClinicApplication.class, args);
+	}
+
+}
